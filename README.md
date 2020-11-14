ae4d293152864f35365fcf508e4478efe07a1d60
johan-v-r <jhnvnrnsbrg@gmail.com>
2020-11-14 09:34:26 +0200

    Add initial source

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LibSassBuilder
+
+> Inspired by [Delegate.SassBuilder](https://github.com/delegateas/Delegate.SassBuilder) and [LibSassHost](https://github.com/Taritsyn/LibSassHost).
+
---
+++ src/LibSassBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using LibSassHost;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LibSassBuilder
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var searchDirectory = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+			Console.WriteLine($"Search Directory: {searchDirectory}");
+
+			var sassFiles = Directory.GetFiles(searchDirectory, "*.scss", SearchOption.AllDirectories);
+
+			foreach (var file in sassFiles)
+			{
+				var fileInfo = new FileInfo(file);
+				if (fileInfo.Name.StartsWith("_")) continue;
+
+				var result = SassCompiler.CompileFile(file, options: new CompilationOptions { OutputStyle = OutputStyle.Compact });
+
+				var newFile = fileInfo.FullName.Replace(fileInfo.Extension, ".css");
+				await File.WriteAllTextAsync(newFile, result.CompiledContent);
+			}
+
+			Console.WriteLine("Sass files compiled");
+		}
+	}
+}
