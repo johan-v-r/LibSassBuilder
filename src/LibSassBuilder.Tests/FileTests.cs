fecab68c3c9a355539655f2f0181127493177b75
johan-v-r <jhnvnrnsbrg@gmail.com>
2020-11-16 11:03:54 +0200

    Compile indented sass format files

--- src/LibSassBuilder.Tests/FileTests.cs
+++ src/LibSassBuilder.Tests/FileTests.cs
@@ -6,11 +6,13 @@ namespace LibSassBuilder.Tests
 	// This project is configured to run LibSassBuilder in LibSassBuilder.Tests.csproj
 	public class FileTests
 	{
-		[Fact]
-		public void FileExistsTest()
+		[Theory]
+		[InlineData("test-new-format.css")]
+		[InlineData("test-indented-format.css")]
+		public void FileExistsTest(string cssFileName)
 		{
 			var fileDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-			var cssFilePath = Path.Join(fileDirectory, "test.css");
+			var cssFilePath = Path.Join(fileDirectory, cssFileName);
 
 			Assert.True(File.Exists(cssFilePath));
 
--- src/LibSassBuilder/Program.cs
+++ src/LibSassBuilder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LibSassHost;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -12,20 +13,32 @@ namespace LibSassBuilder
 			var searchDirectory = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
 			Console.WriteLine($"Sass compile directory: {searchDirectory}");
 
-			var sassFiles = Directory.GetFiles(searchDirectory, "*.scss", SearchOption.AllDirectories);
+			var scssFiles = Directory.GetFiles(searchDirectory, "*.scss", SearchOption.AllDirectories);
+			var sassFiles = Directory.GetFiles(searchDirectory, "*.sass", SearchOption.AllDirectories);
 
+			await CompileFilesAsync(scssFiles);
+			await CompileFilesAsync(sassFiles);
+
+			Console.WriteLine("Sass files compiled");
+		}
+
+		static async Task CompileFilesAsync(IEnumerable<string> sassFiles)
+		{
 			foreach (var file in sassFiles)
 			{
 				var fileInfo = new FileInfo(file);
-				if (fileInfo.Name.StartsWith("_")) continue;
+				if (fileInfo.Name.StartsWith("_"))
+					continue;
 
-				var result = SassCompiler.CompileFile(file, options: new CompilationOptions { OutputStyle = OutputStyle.Compact });
+				var result = SassCompiler.CompileFile(file, options: new CompilationOptions { OutputStyle = OutputStyle.Compressed });
 
 				var newFile = fileInfo.FullName.Replace(fileInfo.Extension, ".css");
+
+				if (File.Exists(newFile) && result.CompiledContent == await File.ReadAllTextAsync(newFile))
+					continue;
+
 				await File.WriteAllTextAsync(newFile, result.CompiledContent);
 			}
-
-			Console.WriteLine("Sass files compiled");
 		}
 	}
 }
