db97ed217f1717da59bba95cd397defa4b13cf09
johan-v-r <jhnvnrnsbrg@gmail.com>
2020-11-17 11:25:50 +0200

    Exclude default directories

--- src/LibSassBuilder.Tests/FileTests.cs
+++ src/LibSassBuilder.Tests/FileTests.cs
@@ -6,17 +6,36 @@ namespace LibSassBuilder.Tests
 	// This project is configured to run LibSassBuilder in LibSassBuilder.Tests.csproj
 	public class FileTests
 	{
+		private readonly string _fileDirectory;
+
+		public FileTests()
+		{
+			_fileDirectory = Path.Join(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "test-files");
+		}
+
 		[Theory]
 		[InlineData("test-new-format.css")]
 		[InlineData("test-indented-format.css")]
 		public void FileExistsTest(string cssFileName)
 		{
-			var fileDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-			var cssFilePath = Path.Join(fileDirectory, cssFileName);
+			var cssFilePath = Path.Join(_fileDirectory, cssFileName);
 
 			Assert.True(File.Exists(cssFilePath));
 
 			File.Delete(cssFilePath);
+		}
+
+		[Theory]
+		[InlineData("", "_ignored.css")]
+		[InlineData("bin", "bin-file.css")]
+		[InlineData("logs", "logs-file.css")]
+		[InlineData("node_modules", "app.css")]
+		[InlineData("obj", "obj-file.css")]
+		public void ExcludedFilesTest(string subFolder, string cssFileName)
+		{
+			var cssFilePath = Path.Join(_fileDirectory, subFolder, cssFileName);
+
+			Assert.False(File.Exists(cssFilePath));
 		}
 	}
 }
--- src/LibSassBuilder/Program.cs
+++ src/LibSassBuilder/Program.cs
@@ -2,24 +2,46 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibSassBuilder
 {
 	class Program
 	{
+		private static readonly List<string> _excludedDirectories = new List<string>
+		{
+			"bin",
+			"obj",
+			"logs",
+			"node_modules"
+		};
+
 		static async Task Main(string[] args)
 		{
 			var searchDirectory = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
 			Console.WriteLine($"Sass compile directory: {searchDirectory}");
 
-			var scssFiles = Directory.GetFiles(searchDirectory, "*.scss", SearchOption.AllDirectories);
-			var sassFiles = Directory.GetFiles(searchDirectory, "*.sass", SearchOption.AllDirectories);
-
-			await CompileFilesAsync(scssFiles);
-			await CompileFilesAsync(sassFiles);
+			await CompileDirectoriesAsync(searchDirectory);
 
 			Console.WriteLine("Sass files compiled");
+		}
+
+		static async Task CompileDirectoriesAsync(string directory)
+		{
+			var sassFiles = Directory.EnumerateFiles(directory)
+				.Where(file => file.EndsWith(".scss", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".sass", StringComparison.OrdinalIgnoreCase));
+
+			await CompileFilesAsync(sassFiles);
+
+			var subDirectories = Directory.EnumerateDirectories(directory);
+			foreach (var subDirectory in subDirectories)
+			{
+				if (_excludedDirectories.Any(dir => subDirectory.EndsWith(dir, StringComparison.OrdinalIgnoreCase)))
+					continue;
+
+				await CompileDirectoriesAsync(subDirectory);
+			}
 		}
 
 		static async Task CompileFilesAsync(IEnumerable<string> sassFiles)
